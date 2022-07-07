49cb3b85949fc72a5cbcfd456aaab2effd428d00
Aamirkhan <94052266+aamirkhan237@users.noreply.github.com>
2022-07-07 14:36:58 +0530

    Add files via upload

---
+++ src/service/cart.service.ts
@@ -0,0 +1,36 @@
+import { CreateUserModel, LoginModel } from "../models/AuthModel";
+import UserModel from "../models/UserModel";
+import request from "./request";
+import axios from "axios";
+
+class AuthService {
+    ENDPOINT = 'api/public';
+
+    public async login(data: LoginModel): Promise<any> {
+        const url = `${this.ENDPOINT}/login`;
+        return request.post(url, data).then((res) => {
+            return res.data.data as UserModel;
+            
+        })
+        .catch((error)=>{            
+           return false;
+
+        });
+    }
+
+    public async create(data: CreateUserModel): Promise<any> {
+        const url = `${this.ENDPOINT}/register`;
+        return request.post<CreateUserModel>(url, data )
+        .then((res) => {
+            
+            return res.data;
+        })
+        .catch((error)=>{
+            
+            return (alert("Invalid format"))
+
+        });
+       
+    }
+}
+export default new AuthService();
